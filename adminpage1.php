1760c3a75c29001eba3ab46038ffb0129902c7db
Aidasalleh <79955619+Aidasalleh@users.noreply.github.com>
2024-12-10 21:34:37 +0800

    Update and rename adminpage1.html to adminpage1.php

--- adminpage1.php
+++ adminpage1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Admin page</title>
     <!-- Link to External CSS File -->
     <link rel="stylesheet" href="admin/adminstyle1.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
