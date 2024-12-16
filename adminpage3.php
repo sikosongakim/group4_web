6a196fe459c662a01f2dccf75881c751832df84b
Aidasalleh <79955619+Aidasalleh@users.noreply.github.com>
2024-12-16 23:52:43 +0800

    Update adminpage3.php

--- adminpage3.php
+++ adminpage3.php
@@ -106,7 +106,7 @@ $leaveRequestsResult = $leaveRequestsStmt->get_result();
     <title>Manage Schedules</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="admin/adminstyle3.css" rel="stylesheet">
+    <link href="admin/adminstyle2.css" rel="stylesheet">
 </head>
 <body>
     <!-- Sidebar -->
@@ -134,12 +134,6 @@ $leaveRequestsResult = $leaveRequestsStmt->get_result();
                 <a href="adminpage3.php" data-tooltip="Manage Schedules">
                     <i class='bx bx-calendar'></i>
                     <span class="links_name">Manage Schedules</span>
-                </a>
-            </li>
-            <li>
-                <a href="#reports" data-tooltip="Reports">
-                    <i class='bx bx-bar-chart'></i>
-                    <span class="links_name">Reports</span>
                 </a>
             </li>
         </ul>
